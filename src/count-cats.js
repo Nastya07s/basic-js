a6d6d4e0a5a11ecfcd30310beaed4dde07ccb48a
Nastya07s <nastya.kotovich07@gmail.com>
2020-02-22 07:45:35 +0300

    feat: resolved count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,8 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  let count = 0;
+  backyard.forEach((array) => array.forEach((elem) => {
+    if(elem === "^^")
+      count++;
+  }));
+  return count;
 };
