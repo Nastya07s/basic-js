d858e5bb67c536f506dffb354fbbe5a06dc581fd
Nastya07s <nastya.kotovich07@gmail.com>
2020-02-22 11:53:45 +0300

    feat: resolve dream team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,11 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(array) {
+  if (!Array.isArray(array)){
+    return false;
+  }
+  let str = [];
+  array.forEach((elem) => {
+    if(typeof(elem) === 'string')
+    str.push(elem.trim()[0].toUpperCase());
+  });
+  return str.sort().join('');
 };
