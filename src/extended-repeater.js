9063dcd7e4db42820db8eae4b72a589a48bafae4
Nastya07s <nastya.kotovich07@gmail.com>
2020-02-24 15:42:55 +0300

    feat: resolve extended repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,36 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+    let result = '';
+    if (options === undefined) {
+        return result;
+    };
+    if (options.repeatTimes !== undefined) {
+        for (let i = 0; i < options.repeatTimes; i++) {
+            result += str;
+            if (options.addition !== undefined) {
+                result = addAdition(result, options);
+            }
+            if (i !== options.repeatTimes - 1)
+                result += options.separator || '+';;
+        }
+    } else {
+        result += str;
+        result = addAdition(result, options)
+    }
+    return result;
 };
-  
+
+function addAdition(result, options) {
+    if (options.additionRepeatTimes !== undefined && options.additionRepeatTimes !== 1) {
+        for (let j = 0; j < options.additionRepeatTimes; j++) {
+            if (j !== options.additionRepeatTimes - 1)
+                result += options.addition + options.additionSeparator || '|';
+            else
+                result += options.addition
+        };
+    } else {
+        result += options.addition;
+    }
+
+    return result;
+}
