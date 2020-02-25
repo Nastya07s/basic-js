ced747c76529511bba4dc783f86d2e614e16b35d
Nastya07s <nastya.kotovich07@gmail.com>
2020-02-25 15:52:24 +0300

    feat: resolve vigenere cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,50 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(mode) {
+        this.mode = mode === undefined || mode === true ? 'direct' : 'reverse';
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    transformKey(key, message, n) {
+        let result = '';
+        let j = 0;
+        for (let i = 0; i < n; i++) {
+            if (message[i].charCodeAt() < 65 || message[i].charCodeAt() > 90) {
+                result += message[i];
+                continue;
+            }
+            if (j === key.length)
+                j = 0;
+            result += key[j];
+            j++;
+        }
+        return result;
+    }
+
+    encrypt(message, key) {
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        this.key = this.transformKey(key, message, message.length);
+        let result = '';
+        for (let i = 0; i < message.length; i++) {
+            if (message[i].charCodeAt() > 64 && message[i].charCodeAt() < 91) {
+                result += String.fromCharCode(((message[i].charCodeAt() - 65 + this.key[i].charCodeAt() - 65) % 26) + 65);
+            } else
+                result += message[i]
+        }
+        return this.mode === 'direct' ? result : result.split('').reverse().join('');
+    }
+
+    decrypt(message, key) {
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        this.key = this.transformKey(key, message, message.length);
+        let result = '';
+        for (let i = 0; i < message.length; i++) {
+            if (message[i].charCodeAt() > 64 && message[i].charCodeAt() < 91) {
+                result += String.fromCharCode(((message[i].charCodeAt() - 64 + 26 - (this.key[i].charCodeAt() - 64)) % 26) + 65);
+            } else
+                result += message[i]
+        }
+        return this.mode === 'direct' ? result : result.split('').reverse().join('');
     }
 }
 
